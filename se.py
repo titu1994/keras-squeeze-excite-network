98f8c0b24d3066bf9ad50f06ba405f44ea5b09c2
Somshubra Majumdar <titu1994@gmail.com>
2019-03-04 10:40:23 -0600

    Add Spatial SE and csSE

--- se.py
+++ se.py
@@ -1,15 +1,18 @@
-from keras.layers import GlobalAveragePooling2D, Reshape, Dense, multiply, Permute
+from keras.layers import GlobalAveragePooling2D, Reshape, Dense, multiply, add, Permute, Conv2D
 from keras import backend as K
 
 
 def squeeze_excite_block(input, ratio=16):
-    ''' Create a squeeze-excite block
+    ''' Create a channel-wise squeeze-excite block
+
     Args:
         input: input tensor
         filters: number of output filters
-        k: width factor
 
     Returns: a keras tensor
+
+    References
+    -   [Squeeze and Excitation Networks](https://arxiv.org/abs/1709.01507)
     '''
     init = input
     channel_axis = 1 if K.image_data_format() == "channels_first" else -1
@@ -26,3 +29,44 @@ def squeeze_excite_block(input, ratio=16):
 
     x = multiply([init, se])
     return x
+
+
+def spatial_squeeze_excite_block(input):
+    ''' Create a spatial squeeze-excite block
+
+    Args:
+        input: input tensor
+
+    Returns: a keras tensor
+
+    References
+    -   [Concurrent Spatial and Channel Squeeze & Excitation in Fully Convolutional Networks](https://arxiv.org/abs/1803.02579)
+    '''
+
+    se = Conv2D(1, (1, 1), activation='sigmoid', use_bias=False,
+                kernel_initializer='he_normal')
+
+    x = multiply([input, se])
+    return x
+
+
+def channel_spatial_squeeze_excite(input, ratio=16):
+    ''' Create a spatial squeeze-excite block
+
+    Args:
+        input: input tensor
+        filters: number of output filters
+
+    Returns: a keras tensor
+
+    References
+    -   [Squeeze and Excitation Networks](https://arxiv.org/abs/1709.01507)
+    -   [Concurrent Spatial and Channel Squeeze & Excitation in Fully Convolutional Networks](https://arxiv.org/abs/1803.02579)
+    '''
+
+    cse = squeeze_excite_block(input, ratio)
+    sse = spatial_squeeze_excite_block(input)
+
+    x = add([cse, sse])
+    return x
+
